7a6fa815c7fee9346a58eae57767217ef1d4483c
albgalhue <albgalhue@alum.us.es>
2022-11-24 20:33:19 +0100

    feat:Añadidas preguntas de si o no y votaciones de si o no Se ha creado las votaciones y preguntas de tipo si o no. También se ha creado en el censo que al seleccionar la votación se elija también el tipo. Falla cuando al acceder a la votación mediante la url 'booth/binaryVoting' no encuentra el archivo.

--- decide/booth/views.py
+++ decide/booth/views.py
@@ -29,3 +29,28 @@ class BoothView(TemplateView):
         context['KEYBITS'] = settings.KEYBITS
 
         return context
+
+class BoothBinaryView(TemplateView):
+    template_name = 'booth/booth.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        vid = kwargs.get('voting_id', 0)
+
+        try:
+            r = mods.get('votingBinary', params={'id': vid})
+
+            # Casting numbers to string to manage in javascript with BigInt
+            # and avoid problems with js and big number conversion
+            for k, v in r[0]['pub_key'].items():
+                r[0]['pub_key'][k] = str(v)
+
+            context['voting'] = json.dumps(r[0])
+        except:
+            raise Http404
+
+        context['KEYBITS'] = settings.KEYBITS
+
+        return context
+
+
